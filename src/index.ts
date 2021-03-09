8c0a2c4e67ce3eca79422b54c01346bc5823e094
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-03-08 20:47:38 -0500

    feat: Added export for QueueEntry for queues that want to type against it.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Mutex } from './Mutex';
 export { default as MutexInterface } from './MutexInterface';
-export { default as Semaphore } from './Semaphore';
+export { default as Semaphore, QueueEntry } from './Semaphore';
 export { default as SemaphoreInterface } from './SemaphoreInterface';
 export { withTimeout } from './withTimeout';
 export { tryAcquire } from './tryAcquire';
